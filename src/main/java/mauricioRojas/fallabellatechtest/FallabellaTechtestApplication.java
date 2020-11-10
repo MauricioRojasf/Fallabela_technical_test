7944c4266948c62efddb164268b6f404bf8b4642
Mauricio Rojas <mauricio.rojasf@usach.cl>
2020-11-09 21:49:34 -0300

    sprinboot proyect initialization

---
+++ src/main/java/mauricioRojas/fallabellatechtest/FallabellaTechtestApplication.java
@@ -0,0 +1,13 @@
+package mauricioRojas.fallabellatechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallabellaTechtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FallabellaTechtestApplication.class, args);
+	}
+
+}
